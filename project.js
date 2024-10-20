ccdb8a16bff517ed9f8b3d9ed10c70a7cb4c0636
Ayushman Rana <ayushmanr621@outlook.com>
2024-10-21 01:33:41 +0530

    added a navbar

--- project.js
+++ project.js
@@ -1,19 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const params = new URLSearchParams(window.location.search);
-    const index = params.get('index');
-    const title = params.get('title');
-    const readme = params.get('readme');
+  const params = new URLSearchParams(window.location.search);
+  const index = params.get("index");
+  const title = params.get("title");
+  const readme = params.get("readme");
 
-    // Set the project title
-    // document.getElementById('project-title').textContent = decodeURIComponent(title);
+  // Set the project title
+  // document.getElementById('project-title').textContent = decodeURIComponent(title);
 
-    // Fetch the README.md file based on the project index
-    fetch(`${readme}`)
-        .then(response => response.text())
-        .then(markdown => {
-            // Use the marked library to convert markdown to HTML
-            const readmeHTML = marked.parse(markdown);
-            document.getElementById('readme-content').innerHTML = readmeHTML;
-        })
-        .catch(error => console.error("Error loading README:", error));
+  // Fetch the README.md file based on the project index
+  fetch(`${readme}`)
+    .then((response) => response.text())
+    .then((markdown) => {
+      // Use the marked library to convert markdown to HTML
+      const readmeHTML = marked.parse(markdown);
+      document.getElementById("readme-content").innerHTML = readmeHTML;
+    })
+    .catch((error) => console.error("Error loading README:", error));
 });
